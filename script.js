572adc3816d4845bc54773d5e0a86667fd15011c
XanderWitch <janell.robisch@gmail.com>
2022-04-01 17:24:30 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.faq-toggle').forEach(item => item.addEventListener('click', event => item.parentNode.classList.toggle('active')))
+
+
+
+//Brad T's Version
+// const toggles = querySelectorAll('.faq-toggle');
+
+// toggles.forEach(toggle => {
+//     toggle.addEventListner('click', () => {
+//         toggle.parentNode.classList.toggle('active')
+//     })
+// })
